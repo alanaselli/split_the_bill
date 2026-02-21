6ed8d937a7467f175b314b20dfac47d306508483
Alana Selli <alana.selli@gmail.com>
2026-02-21 10:57:06 -0300

    first class

--- src/main.py
+++ src/main.py
@@ -1,5 +1,10 @@
+class Split:
+    def __init__(self):
+        pass
+
 def main():
-    print("Hello, World!")
+    split_obj = Split()
+    print(split_obj)
 
 if __name__ == "__main__":
     main()
