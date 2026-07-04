a2277cb379bfd02836079e51074917b63a88f738
Alana Selli <alana.selli@gmail.com>
2026-07-04 11:57:17 -0300

    Added Ledger class and its tests

--- src/main.py
+++ src/main.py
@@ -7,16 +7,40 @@
 # - only two people are involved
 
 class Bill:
-    def __init__(self, amount=0, paid_by=None):
+    def __init__(self, amount=0, paid_by=None, shared_with=None, is_settled=False):
         self.amount = amount
         self.paid_by = paid_by
+        self.shared_with = shared_with
+        self.is_settled = is_settled
 
         self.receives = self.amount/2
+    
+    def settle(self):
+        self.is_settled = True
+
+class Ledger:
+    def __init__(self):
+        self.list_of_bills = []
+        self.balance = {}
+
+    def add_bill(self, bill):
+        self.list_of_bills.append(bill)
+
+    def get_balances(self):
+
+        for bill in self.list_of_bills:
+            self.balance.setdefault(bill.paid_by, 0)
+            self.balance.setdefault(bill.shared_with, 0)
+
+            self.balance[bill.paid_by] += bill.amount/2
+            self.balance[bill.shared_with] -= bill.amount/2
+
 
 def main():
-    bill_01 = Bill(5, paid_by='person1')
+    bill_01 = Bill(5, paid_by='person1', shared_with='person2')
     print("Amount: ", bill_01.amount)
     print("Paid by: ", bill_01.paid_by)
+    print("Shared with: ", bill_01.shared_with)
     print("Receives: ", bill_01.receives)
 
 
--- tests/test_01.py
+++ tests/test_01.py
@@ -1,6 +1,6 @@
 import unittest  # https://docs.python.org/3/library/unittest.html
 
-from src.main import Bill
+from src.main import Bill, Ledger
 
 class TestBill(unittest.TestCase):
     def test_default_amount(self):
@@ -14,6 +14,46 @@ class TestBill(unittest.TestCase):
     def test_division(self):
         bill = Bill(50, "Person A")
         self.assertEqual(bill.receives, 25)
+
+    def test_is_settled_default(self):
+        bill = Bill()
+        self.assertFalse(bill.is_settled)
+
+    def test_is_settled(self):
+        bill = Bill()
+        bill.settle()
+        self.assertTrue(bill.is_settled)
+
+class TestLedger(unittest.TestCase):
+    def test_add_bill(self):
+        bill = Bill(amount=50, paid_by="Person A", shared_with="Person B")
+        
+        ledger = Ledger()
+        ledger.add_bill(bill)
+
+        self.assertListEqual(ledger.list_of_bills, [bill])
+
+    def test_get_balances(self):
+        bill = Bill(amount=50, paid_by="Person A", shared_with="Person B")
+
+        ledger = Ledger()
+        ledger.add_bill(bill)
+
+        ledger.get_balances()
+
+        self.assertDictEqual(ledger.balance, {"Person A": 25, "Person B": -25})
+
+    def test_get_balances_2(self):
+        bill_01 = Bill(amount=50, paid_by="Person A", shared_with="Person B")
+        bill_02 = Bill(amount=20, paid_by="Person B", shared_with="Person A")
+
+        ledger = Ledger()
+        ledger.add_bill(bill_01)
+        ledger.add_bill(bill_02)
+
+        ledger.get_balances()
+
+        self.assertDictEqual(ledger.balance, {"Person A": 15, "Person B": -15})
 
 if __name__ == "__main__":
     unittest.main()
